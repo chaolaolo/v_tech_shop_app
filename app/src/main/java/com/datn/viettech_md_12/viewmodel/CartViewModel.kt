18f5301eee545c915ee1e6ed832a9df6770ad314
Admin <k3.a4.tuananhh@gmail.com>
2025-03-29 12:02:37 +0700

    Change AsyncImage

--- app/src/main/java/com/datn/viettech_md_12/viewmodel/CartViewModel.kt
+++ app/src/main/java/com/datn/viettech_md_12/viewmodel/CartViewModel.kt
@@ -5,10 +5,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.datn.viettech_md_12.data.model.CartModel
 import com.datn.viettech_md_12.data.remote.ApiClient
+import com.google.gson.JsonSyntaxException
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 import retrofit2.Response
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 
 class CartViewModel : ViewModel() {
@@ -19,22 +23,58 @@ class CartViewModel : ViewModel() {
 
     private val _updateCartState = MutableStateFlow<Response<CartModel>?>(null)
     val updateCartState: StateFlow<Response<CartModel>?> get() = _updateCartState
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean> = _isLoading
 
-    fun fetchCart(token:String, userId:String, userIdQuery:String) {
+//    fun fetchCart(token:String, userId:String, userIdQuery:String) {
+//        viewModelScope.launch {
+//            try {
+//                val response = cartRepository.getCart(
+//                    token = token,
+//                    userId = userId,
+//                    userIdQuery=userIdQuery
+//                )
+//                _cartState.value = response
+//                Log.d("CartViewModel", "Fetch Cart Success: ${response.body()}")
+//            } catch (e: Exception) {
+//                Log.e("CartViewModel", "Fetch Cart Error: ${e.message}", e)
+//            }
+//        }
+//    }
+
+    fun fetchCart(token: String, userId: String, userIdQuery: String) {
         viewModelScope.launch {
+            _isLoading.value = true
             try {
                 val response = cartRepository.getCart(
                     token = token,
                     userId = userId,
-                    userIdQuery=userIdQuery
+                    userIdQuery = userIdQuery
                 )
-                _cartState.value = response
-                Log.d("CartViewModel", "Fetch Cart Success: ${response.body()}")
+                if (response.isSuccessful) {
+                    _cartState.value = response
+                    response.body()?.let {
+                        Log.d("dm", "Fetch Cart Success: ${it}")
+                    }
+                } else {
+                    Log.e("dm", "Fetch Cart Failed: ${response.code()} - ${response.message()}")
+                }
+            } catch (e: UnknownHostException) {
+                Log.e("dm_error", "Lỗi mạng: Không thể kết nối với máy chủ")
+            } catch (e: SocketTimeoutException) {
+                Log.e("dm_error", "Lỗi mạng: Đã hết thời gian chờ")
+            } catch (e: HttpException) {
+                Log.e("dm_error", "Lỗi HTTP: ${e.message()}")
+            } catch (e: JsonSyntaxException) {
+                Log.e("dm_error", "Lỗi dữ liệu: Invalid JSON response")
             } catch (e: Exception) {
-                Log.e("CartViewModel", "Fetch Cart Error: ${e.message}", e)
+                Log.e("dm_error", "Lỗi chung: ${e.message}", e)
+            } finally {
+                _isLoading.value = false // Kết thúc trạng thái loading
             }
         }
     }
+
 
 
 
